a60eaf0edca6ea407879deab6f3b36605de14b77
shunlog <artiombalan331@gmail.com>
2024-04-22 10:55:51 +0300

    mockup the journal

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import img1 from './assets/journal/castle.jpg'
+import img2 from './assets/journal/beach.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <article className="journal">
+        <h1>12th April, 2021</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis tincidunt id aliquet risus feugiat in ante metus. Arcu ac tortor dignissim convallis aenean et. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis id consectetur purus ut faucibus pulvinar. Vestibulum sed arcu non odio euismod lacinia at quis risus. Viverra suspendisse potenti nullam ac tortor vitae. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Elementum facilisis leo vel fringilla. Porta lorem mollis aliquam ut porttitor leo. Augue mauris augue neque gravida in fermentum.
+        </p>
+        <img src={img1} alt="" />
+        <p>
+          Sagittis vitae et leo duis ut. Sem nulla pharetra diam sit amet. Vestibulum lorem sed risus ultricies tristique nulla. A erat nam at lectus urna duis convallis convallis tellus. In pellentesque massa placerat duis ultricies lacus sed turpis tincidunt. Nulla facilisi cras fermentum odio eu feugiat pretium nibh. Praesent semper feugiat nibh sed. Eget velit aliquet sagittis id consectetur. Aliquam eleifend mi in nulla. Aliquam malesuada bibendum arcu vitae elementum curabitur.
+        </p>
+        <p>
+          Aliquam vestibulum morbi blandit cursus risus at ultrices. Elementum tempus egestas sed sed risus pretium quam. Quis blandit turpis cursus in hac habitasse platea dictumst. Urna neque viverra justo nec ultrices dui sapien eget mi. Etiam tempor orci eu lobortis elementum nibh. Sed euismod nisi porta lorem mollis. Elementum facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Facilisis mauris sit amet massa vitae tortor condimentum lacinia. A erat nam at lectus urna duis convallis convallis. Molestie nunc non blandit massa enim nec dui nunc mattis.
+        </p>
+        <p>
+          Tortor at auctor urna nunc id cursus metus. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit. Tempor orci dapibus ultrices in iaculis. Commodo nulla facilisi nullam vehicula ipsum. Ornare suspendisse sed nisi lacus sed viverra tellus. Cras sed felis eget velit. Felis eget velit aliquet sagittis id consectetur purus ut. Nibh ipsum consequat nisl vel. Lectus urna duis convallis convallis tellus id interdum velit laoreet. Elementum tempus egestas sed sed. Luctus accumsan tortor posuere ac ut consequat semper viverra. Posuere sollicitudin aliquam ultrices sagittis orci. Neque viverra justo nec ultrices dui sapien eget mi. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum. Vestibulum sed arcu non odio euismod lacinia at. Tortor at risus viverra adipiscing. Erat pellentesque adipiscing commodo elit at imperdiet. Magna fermentum iaculis eu non. Vulputate ut pharetra sit amet. Id diam vel quam elementum pulvinar etiam non.
+        </p>
+        <img src={img2} alt="" />
+        <p>
+          Diam phasellus vestibulum lorem sed risus ultricies tristique. Interdum velit euismod in pellentesque massa placerat duis. Libero id faucibus nisl tincidunt eget nullam. Ut etiam sit amet nisl purus in. Ornare suspendisse sed nisi lacus sed viverra tellus. Ac tortor dignissim convallis aenean et tortor at. At tempor commodo ullamcorper a lacus vestibulum sed arcu non. Quis blandit turpis cursus in hac habitasse. Iaculis urna id volutpat lacus. Pharetra massa massa ultricies mi. Semper feugiat nibh sed pulvinar. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Enim nec dui nunc mattis enim.
+        </p>
+
+      </article>
+    </>
+  )
+}
+
+export default App
