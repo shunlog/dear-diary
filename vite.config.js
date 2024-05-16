e2289577ca5cf4f7f7186ae343723ca5a800a0ca
shunlog <artiombalan331@gmail.com>
2024-05-16 14:48:51 +0300

    feature: import markdown file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      strict: false,
+allow: [
+        // search up for workspace root
+        // searchForWorkspaceRoot(process.cwd()),
+        // your custom rules
+        '/home/awh/Downloads/journal',
+      ],
+    }
+  },
 })
