48b8706c1c3a7e56e127efa93296374679df366b
shunlog <artiombalan331@gmail.com>
2024-05-16 22:57:48 +0300

    feature: upload images

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,73 @@ import { useState } from 'react'
 import './App.css'
 import showdown  from 'showdown'
 
-function App() {
-  const [htmlText, setHtmlText] = useState();
+const readFile = (f, callback) => {
+  // start asynchronously reading the file
+  // and call the callback function with the result when finished
+  const reader = new FileReader();
+  reader.addEventListener("loadend", () => {
+    callback(reader.result);
+  });
+  reader.readAsText(f);
+}
 
-  // setHtmlText("<h1>Import a markdown file</h1>");
+/**
+ * @param {String} HTML representing a single element.
+ * @param {Boolean} flag representing whether or not to trim input whitespace, defaults to true.
+ * @return {Element | null}
+ */
+function strToHTML(html, trim = true) {
+  // https://stackoverflow.com/a/35385518
+  // Process the HTML string.
+  html = trim ? html.trim() : html;
+  if (!html) return null;
+
+  // Then set up a new template element.
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  const result = template.content;
+
+  // if there's multiple children, wrap them in a div Element
+  if (result.length === 1) return result[0];
+
+  let div = document.createElement("div");
+  div.append(result);
+  return div;
+}
+
+function App() {
+  const [journalStr, setJournalStr] = useState("");
+
+  const updateMarkdownText = (text) => {
+    // convert the markdown text to html
+    let converter = new showdown.Converter({
+      tables: true
+    });
+    let html_str = converter.makeHtml(text);
+
+    setJournalStr(html_str);
+  }
+
+  const handleImgsUpload = (e) => {
+    const files = e.target.files;
+    const html = strToHTML(journalStr);
+
+    Array.from(html.getElementsByTagName("img")).map((img) =>{
+      const fn  = img.src.split("/").at(-1);
+      const file = Array.from(files).find((f) => f.name == fn);
+      if (file === undefined) return;
+      const url = URL.createObjectURL(file);
+      img.src = url;
+    });
+
+    setJournalStr(html.innerHTML);
+  };
 
   const handleFileChange = (e) => {
     if (!e.target.files) return;
-
-    let f = e.target.files[0]
-
-    const reader = new FileReader();
-    reader.addEventListener("loadend", () => {
-      let text = reader.result;
-      let converter = new showdown.Converter();
-      let html      = converter.makeHtml(text);
-      setHtmlText(html);
-      console.log(html);
-    });
-    reader.readAsText(f);
+    readFile(e.target.files[0], updateMarkdownText);
   };
+
 
   return (
     <>
@@ -29,7 +76,11 @@ function App() {
         Import file: <input type="file" onChange={handleFileChange} />
       </p>
 
-      <article className="journal" dangerouslySetInnerHTML={{ __html: htmlText }}>
+      <p>
+        Select images: <input type="file" id="input" multiple onChange={handleImgsUpload} />
+      </p>
+
+      <article className="journal" dangerouslySetInnerHTML={{ __html: journalStr }}>
       </article>
     </>
   )
